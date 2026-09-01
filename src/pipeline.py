7293e759e876666bf340e4b0e29c031e33b55b48
kfloresdacosta <k.floresdacosta@ecoles-epsi.net>
2026-09-01 15:46:17 +0200

    Refactor weather data extraction for improved readability

--- src/pipeline.py
+++ src/pipeline.py
@@ -47,68 +47,22 @@ def main() -> None:
                     "population": city["population"],
                     "department_code": city["department_code"],
                     "region_code": city["region_code"],
-
                     "timestamp": hourly["time"][i],
-
-                    "temperature": hourly[
-                        "temperature_2m"
-                    ][i],
-
-                    "humidity": hourly[
-                        "relative_humidity_2m"
-                    ][i],
-
-                    "apparent_temperature": hourly[
-                        "apparent_temperature"
-                    ][i],
-
-                    "precipitation": hourly[
-                        "precipitation"
-                    ][i],
-
-                    "precipitation_probability": hourly[
-                        "precipitation_probability"
-                    ][i],
-
-                    "rain": hourly[
-                        "rain"
-                    ][i],
-
-                    "showers": hourly[
-                        "showers"
-                    ][i],
-
-                    "snowfall": hourly[
-                        "snowfall"
-                    ][i],
-
-                    "cloud_cover": hourly[
-                        "cloud_cover"
-                    ][i],
-
-                    "pressure": hourly[
-                        "pressure_msl"
-                    ][i],
-
-                    "wind_speed": hourly[
-                        "wind_speed_10m"
-                    ][i],
-
-                    "wind_direction": hourly[
-                        "wind_direction_10m"
-                    ][i],
-
-                    "wind_gusts": hourly[
-                        "wind_gusts_10m"
-                    ][i],
-
-                    "uv_index": hourly[
-                        "uv_index"
-                    ][i],
-
-                    "weather_code": hourly[
-                        "weather_code"
-                    ][i],
+                    "temperature": hourly["temperature_2m"][i],
+                    "humidity": hourly["relative_humidity_2m"][i],
+                    "apparent_temperature": hourly["apparent_temperature"][i],
+                    "precipitation": hourly["precipitation"][i],
+                    "precipitation_probability": hourly["precipitation_probability"][i],
+                    "rain": hourly["rain"][i],
+                    "showers": hourly["showers"][i],
+                    "snowfall": hourly["snowfall"][i],
+                    "cloud_cover": hourly["cloud_cover"][i],
+                    "pressure": hourly["pressure_msl"][i],
+                    "wind_speed": hourly["wind_speed_10m"][i],
+                    "wind_direction": hourly["wind_direction_10m"][i],
+                    "wind_gusts": hourly["wind_gusts_10m"][i],
+                    "uv_index": hourly["uv_index"][i],
+                    "weather_code": hourly["weather_code"][i],
                 }
             )
 
